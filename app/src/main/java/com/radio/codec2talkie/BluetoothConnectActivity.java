cb3afe1e07fd18a8ee7bcf368723b83a0019c0a7
sh123 <sh123@users.noreply.github.com>
2020-11-28 17:00:23 +0200

    Add bluetotoh intent

---
+++ app/src/main/java/com/radio/codec2talkie/BluetoothConnectActivity.java
@@ -0,0 +1,14 @@
+package com.radio.codec2talkie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BluetoothConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_connect);
+    }
+}
--- app/src/main/java/com/radio/codec2talkie/MainActivity.java
+++ app/src/main/java/com/radio/codec2talkie/MainActivity.java
@@ -2,6 +2,7 @@ package com.radio.codec2talkie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startBluetoothConnectActivity();
+    }
+
+    protected void startBluetoothConnectActivity() {
+        Intent intent = new Intent(this, BluetoothConnectActivity.class);
+        startActivity(intent);
     }
 }
