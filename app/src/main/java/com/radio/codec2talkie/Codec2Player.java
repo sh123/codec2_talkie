f1c5712d195bef725a268a922a0e7a960569d578
sh123 <sh123@users.noreply.github.com>
2020-12-02 18:27:58 +0200

    Implement KISS frame decoding

--- app/src/main/java/com/radio/codec2talkie/Codec2Player.java
+++ app/src/main/java/com/radio/codec2talkie/Codec2Player.java
@@ -15,6 +15,24 @@ import com.ustadmobile.codec2.Codec2;
 
 public class Codec2Player extends Thread {
 
+    private final byte KISS_FEND = (byte)0xc0;
+    private final byte KISS_FESC = (byte)0xdb;
+    private final byte KISS_TFEND = (byte)0xdc;
+    private final byte KISS_TFESC = (byte)0xdd;
+
+    private final byte KISS_CMD_DATA = (byte)0x00;
+    private final byte KISS_CMD_NOCMD = (byte)0x80;
+
+    private enum KissState {
+        VOID,
+        GET_CMD,
+        GET_DATA,
+        ESCAPE
+    };
+
+    private KissState _kissState = KissState.VOID;
+    private byte _kissCmd = KISS_CMD_NOCMD;
+
     private final InputStream _btInputStream;
     private final OutputStream _btOutputStream;
 
@@ -64,44 +82,109 @@ public class Codec2Player extends Thread {
         _codec2Con = Codec2.create(Codec2.CODEC2_MODE_1200);
     }
 
+    private void processRecording() {
+        int audioBufferSize = Codec2.getSamplesPerFrame(_codec2Con);
+        int encodedBufferSize = (Codec2.getBitsSize(_codec2Con) + 7) / 8;
+
+        short[] recordAudioBuffer = new short[audioBufferSize];
+        char[] recordEncodedBuffer = new char[encodedBufferSize];
+
+        _audioRecorder.read(recordAudioBuffer, 0, audioBufferSize);
+        Codec2.encode(_codec2Con, recordAudioBuffer, recordEncodedBuffer);
+
+        try {
+            _btOutputStream.write(KISS_FEND);
+            _btOutputStream.write(KISS_CMD_DATA);
+            for (char c : recordEncodedBuffer) {
+                byte b = (byte)c;
+                if (b == KISS_FEND) {
+                    _btOutputStream.write(KISS_FESC);
+                    _btOutputStream.write(KISS_TFEND);
+                } else if (b == KISS_FESC){
+                    _btOutputStream.write(KISS_FESC);
+                    _btOutputStream.write(KISS_TFESC);
+                } else {
+                    _btOutputStream.write(b);
+                }
+                _btOutputStream.write(KISS_FEND);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processPlayback() {
+        try {
+            int btBytes = _btInputStream.available();
+            if (btBytes > 0) {
+                byte[] br = new byte[1];
+                int bytesRead = _btInputStream.read(br);
+                if (bytesRead == 1) {
+                    byte b = br[0];
+                    switch (_kissState) {
+                        case VOID:
+                            if (b == KISS_FEND) {
+                                _kissCmd = KISS_CMD_NOCMD;
+                                _kissState = KissState.GET_CMD;
+                            }
+                            break;
+                        case GET_CMD:
+                            if (b != KISS_FEND) {
+                                if (b == KISS_CMD_DATA) {
+                                    // reset buffer
+                                    _kissCmd = b;
+                                    _kissState = KissState.GET_DATA;
+                                } else {
+                                    _kissCmd = KISS_CMD_NOCMD;
+                                    _kissState = KissState.VOID;
+                                }
+                            }
+                            break;
+                        case GET_DATA:
+                            if (b == KISS_FESC) {
+                                _kissState = KissState.ESCAPE;
+                            } else if (b == KISS_FEND) {
+                                if (_kissCmd == KISS_CMD_DATA) {
+                                    // decode buffer and write to audio out
+                                }
+                                _kissCmd = KISS_CMD_NOCMD;
+                                _kissState = KissState.VOID;
+                            } else {
+                                // add byte to decode buffer
+                            }
+                            break;
+                        case ESCAPE:
+                            if (b == KISS_TFEND) {
+                                // write FEND to buffer
+                                _kissState = KissState.GET_DATA;
+                            }
+                            else if (b == KISS_TFESC) {
+                                // write FESC to buffer
+                                _kissState = KissState.GET_DATA;
+                            }
+                            else {
+                                _kissCmd = KISS_CMD_NOCMD;
+                                _kissState = KissState.VOID;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void run() {
         while (true) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            // read from mic, encode and write to bluetooth
             if (_audioRecorder.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
-                int audioBufferSize = Codec2.getSamplesPerFrame(_codec2Con);
-                int encodedBufferSize = (Codec2.getBitsSize(_codec2Con) + 7) / 8;
-
-                short[] recordAudioBuffer = new short[audioBufferSize];
-                char[] recordEncodedBuffer = new char[encodedBufferSize];
-
-                _audioRecorder.read(recordAudioBuffer, 0, audioBufferSize);
-                Codec2.encode(_codec2Con, recordAudioBuffer, recordEncodedBuffer);
-
-                // encode with KISS and send to bluetooth
-                try {
-                    for (char b : recordEncodedBuffer) {
-                        _btOutputStream.write(b);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            } else {
-                try {
-                    int btBytes = _btInputStream.available();
-                    if (btBytes > 0) {
-
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                processRecording();
+            }
+            else {
+                processPlayback();
             }
         }
     }
