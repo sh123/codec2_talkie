b6e86183ba7cd2a2251367971b5043b8c6c36602
sh123 <sh123@users.noreply.github.com>
2021-10-10 21:13:23 +0300

    Added preference keys

--- codec2talkie/src/main/java/com/radio/codec2talkie/settings/PreferenceKeys.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/settings/PreferenceKeys.java
@@ -3,6 +3,9 @@ package com.radio.codec2talkie.settings;
 public final class PreferenceKeys {
     public static String PORTS_USB_SERIAL_SPEED = "ports_usb_serial_speed";
     public static String PORTS_BT_CLIENT_NAME = "ports_bt_client_name";
+    public static String PORTS_TCP_IP_ENABLED = "ports_tcp_ip_enable";
+    public static String PORTS_TCP_IP_ADDRESS = "ports_tcp_ip_address";
+    public static String PORTS_TCP_IP_PORT = "ports_tcp_ip_port";
 
     public static String CODEC2_MODE = "codec2_mode";
     public static String CODEC2_TEST_MODE = "codec2_test_mode";
