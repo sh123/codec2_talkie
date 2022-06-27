1a5b7ffba56581661df3797c779f9cc47c978b2c
sh123 <sh123@users.noreply.github.com>
2022-06-27 23:50:09 +0300

    Implement callsign class

--- codec2talkie/src/main/java/com/radio/codec2talkie/protocol/ax25/AX25Callsign.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/protocol/ax25/AX25Callsign.java
@@ -1,29 +1,76 @@
 package com.radio.codec2talkie.protocol.ax25;
 
+import java.nio.ByteBuffer;
+
 public class AX25Callsign {
+    public final int CallsignMaxSize = 7;
+
     public String callsign;
-    public String ssid;
+    public int ssid;
     public boolean isValid;
 
     public void fromString(String callsignWithSsid) {
-        String[] callsignSsid = callsignWithSsid.split("-", 2);
-        if (callsignSsid.length == 2) {
-            callsign = callsignSsid[0];
-            ssid = callsignSsid[1];
-            isValid = true;
+        isValid = false;
+        // ABCDEF-XX
+        if (callsignWithSsid.length() > CallsignMaxSize + 2 || callsignWithSsid.length() == 0) return;
+        int delimiterIndex = callsignWithSsid.indexOf('-');
+        // ABCDEF-
+        if (delimiterIndex != -1 && delimiterIndex == callsignWithSsid.length() - 1) return;
+        callsign = callsignWithSsid;
+        ssid = 0;
+        if (delimiterIndex == -1) {
+            // ABCDEF
+            if (callsign.length() >= CallsignMaxSize) return;
         } else {
-            isValid = false;
+            callsign = callsignWithSsid.substring(0, delimiterIndex);
+            try {
+                ssid = Integer.parseInt(callsignWithSsid.substring(delimiterIndex + 1));
+            } catch(NumberFormatException e) {
+                return;
+            }
         }
+        isValid = true;
     }
 
     public void fromBinary(byte[] data) {
+        isValid = false;
+        if (data.length != CallsignMaxSize) return;
+
+        char[] buffer = new char[CallsignMaxSize];
+
+        for (int i = 0; i < data.length - 1; i++) {
+            char c = (char)(data[i] >> 1);
+            if (c == ' '){
+                buffer[i] = '\0';
+            } else {
+                buffer[i] = c;
+            }
+        }
+        buffer[CallsignMaxSize - 1] = '\0';
+
+        callsign = new String(buffer);
+        ssid = (data[data.length - 1] >> 1) & 0x0f;
+
+        if (callsign.length() == 0) return;
+        isValid = true;
     }
 
-    public String toString() {
+    public String toStr() {
         return callsign + "-" + ssid;
     }
 
     public byte[] toBinary() {
-        return null;
+        ByteBuffer buffer = ByteBuffer.allocate(CallsignMaxSize);
+        for (int i = 0; i < CallsignMaxSize - 1; i++) {
+            if (i < callsign.length()) {
+                byte c = (byte) callsign.charAt(i);
+                buffer.put((byte) (c << 1));
+            } else {
+                // append ' ' for short callsigns
+                buffer.put((byte)(0x20 << 1));
+            }
+            buffer.put((byte)(ssid << 1));
+        }
+        return buffer.array();
     }
 }
