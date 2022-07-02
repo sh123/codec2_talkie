fed951058e3327f4856fe0e9656070ec0e47a623
sh123 <sh123@users.noreply.github.com>
2022-07-02 19:17:22 +0300

    Implemented compressed coordinates conversion

--- codec2talkie/src/main/java/com/radio/codec2talkie/protocol/aprs/AprsDataPositionReport.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/protocol/aprs/AprsDataPositionReport.java
@@ -1,6 +1,7 @@
 package com.radio.codec2talkie.protocol.aprs;
 
 import com.radio.codec2talkie.protocol.position.Position;
+import com.radio.codec2talkie.tools.MathTools;
 import com.radio.codec2talkie.tools.UnitTools;
 
 import java.nio.ByteBuffer;
@@ -16,23 +17,9 @@ public class AprsDataPositionReport implements AprsData {
     public void fromPosition(Position position) {
         _isValid = false;
         _position = position;
-        ByteBuffer buffer = ByteBuffer.allocate(256);
-        buffer.putChar('!');
-        buffer.put(getUncompressedNmeaCoordinate(position).getBytes());
-        // put course altitude
-        if (position.isSpeedBearingEnabled) {
-            buffer.put(String.format(Locale.US, "%03d/%03d",
-                    (int)position.bearingDegrees,
-                    UnitTools.metersPerSecondToKnots(position.speedMetersPerSecond)).getBytes());
-        }
-        if (position.isAltitudeEnabled && position.altitudeMeters >= 0) {
-            buffer.put(String.format(Locale.US, "/A=%05d",
-                    UnitTools.metersToFeet(position.altitudeMeters)).getBytes());
-        }
-        buffer.put(position.comment.getBytes());
-        buffer.flip();
-        _binary = new byte[buffer.remaining()];
-        buffer.get(_binary);
+        _binary = position.isCompressed
+                ? generateCompressedInfo(position)
+                : generateUncompressedInfo(position);
         _isValid = true;
     }
 
@@ -44,14 +31,16 @@ public class AprsDataPositionReport implements AprsData {
     @Override
     public void fromBinary(byte[] infoData) {
         _isValid = false;
-        ByteBuffer buffer = ByteBuffer.wrap(infoData);
+        // TODO
+        // ByteBuffer buffer = ByteBuffer.wrap(infoData);
         // read latitude
         // read symbol table
         // read longitude
         // read symbol
-        // read altitude, course
+        // read course/speed
+        // read altitude (could be anywhere inside the comment)
         // read comment until the end
-        _isValid = true;
+        // _isValid = true;
     }
 
     @Override
@@ -64,18 +53,98 @@ public class AprsDataPositionReport implements AprsData {
         return _isValid;
     }
 
+    private byte[] generateCompressedInfo(Position position) {
+        ByteBuffer buffer = ByteBuffer.allocate(256);
+        buffer.putChar('!');
+        buffer.put(getCompressedNmeaCoordinate(position));
+        // compressed can hold either speed and bearing or altitude
+        if (position.isSpeedBearingEnabled) {
+            buffer.put((byte)(33 + (byte)(position.bearingDegrees / 4.0)));
+            double compressedSpeed = MathTools.log(1.08, 1 + UnitTools.metersPerSecondToKnots(position.speedMetersPerSecond));
+            buffer.put((byte)(33 + (byte)(compressedSpeed)));
+            buffer.putChar('[');
+        } else if (position.isAltitudeEnabled) {
+            double compressedAltitude = MathTools.log(1.002, UnitTools.metersToFeet(position.altitudeMeters));
+            buffer.put((byte)(33 + (byte)(compressedAltitude / 100)));
+            buffer.put((byte)(33 + (byte)(compressedAltitude % 100)));
+            buffer.putChar('T');
+        } else {
+            buffer.put(" sT".getBytes());
+        }
+        buffer.put(position.comment.getBytes());
+        // return
+        buffer.flip();
+        byte [] binaryInfo = new byte[buffer.remaining()];
+        buffer.get(binaryInfo);
+        return binaryInfo;
+    }
+
+    private byte[] generateUncompressedInfo(Position position) {
+        ByteBuffer buffer = ByteBuffer.allocate(256);
+        buffer.putChar('!');
+        buffer.put(getUncompressedNmeaCoordinate(position).getBytes());
+        // put course altitude
+        if (position.isSpeedBearingEnabled) {
+            buffer.put(String.format(Locale.US, "%03d/%03d",
+                    (int) position.bearingDegrees,
+                    UnitTools.metersPerSecondToKnots(position.speedMetersPerSecond)).getBytes());
+        }
+        if (position.isAltitudeEnabled && position.altitudeMeters >= 0) {
+            buffer.put(String.format(Locale.US, "/A=%05d",
+                    UnitTools.metersToFeet(position.altitudeMeters)).getBytes());
+        }
+        buffer.put(position.comment.getBytes());
+        // return
+        buffer.flip();
+        byte [] binaryInfo = new byte[buffer.remaining()];
+        buffer.get(binaryInfo);
+        return binaryInfo;
+    }
+
     private String getUncompressedNmeaCoordinate(Position position) {
-        String latitude = applyPrivacyOnNmeaCoordinate(
+        String latitude = applyPrivacyOnUncompressedNmeaCoordinate(
                 UnitTools.decimalToNmea(position.latitude, true),
                 position.privacyLevel);
-        String longitude = applyPrivacyOnNmeaCoordinate(
+        String longitude = applyPrivacyOnUncompressedNmeaCoordinate(
                 UnitTools.decimalToNmea(position.latitude, false),
                 position.privacyLevel);
         byte[] symbol = position.symbolCode.getBytes();
         return String.format(Locale.US, "%s%c%s%c", latitude, symbol[0], longitude, symbol[1]);
     }
 
-    public static String applyPrivacyOnNmeaCoordinate(String nmeaCoordinate, int privacyLevel) {
+    private byte[] getCompressedNmeaCoordinate(Position position) {
+        byte[] symbol = position.symbolCode.getBytes();
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        // symbol table
+        buffer.put(symbol[0]);
+        // latitude
+        double lat = Math.abs(380926 * (position.latitude - 90.0));
+        buffer.put((byte)(33 + (byte)(lat / (91 * 91 * 91))));
+        lat %= (91 * 91 * 91);
+        buffer.put((byte)(33 + (byte)(lat / (91 * 91))));
+        lat %= (91 * 91);
+        buffer.put((byte)(33 + (byte)(lat / (91))));
+        lat %= (91);
+        buffer.put((byte)(33 + (byte)lat));
+        // longitude
+        double lon = Math.abs(190463 * (position.longitude + 180.0));
+        buffer.put((byte)(33 + (byte)(lon / (91 * 91 * 91))));
+        lon %= (91 * 91 * 91);
+        buffer.put((byte)(33 + (byte)(lon / (91 * 91))));
+        lon %= (91 * 91);
+        buffer.put((byte)(33 + (byte)(lon / (91))));
+        lon %= (91);
+        buffer.put((byte)(33 + (byte)lon));
+        // symbol
+        buffer.put(symbol[1]);
+        // return
+        buffer.flip();
+        byte [] binaryCoordinate = new byte[buffer.remaining()];
+        buffer.get(binaryCoordinate);
+        return binaryCoordinate;
+    }
+
+    public static String applyPrivacyOnUncompressedNmeaCoordinate(String nmeaCoordinate, int privacyLevel) {
         byte [] buffer = nmeaCoordinate.getBytes();
         int level = 0;
         for (int i = buffer.length - 2; i > 0 && level < privacyLevel; i--) {
---
+++ codec2talkie/src/main/java/com/radio/codec2talkie/tools/MathTools.java
@@ -0,0 +1,7 @@
+package com.radio.codec2talkie.tools;
+
+public class MathTools {
+    public static double log(double base, double logNumber) {
+        return Math.log(logNumber) / Math.log(base);
+    }
+}
