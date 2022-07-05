428be9bdbef893848d8a05d143296e6a8de823bc
sh123 <sh123@users.noreply.github.com>
2022-07-05 15:36:41 +0300

    Cleanup

--- codec2talkie/src/main/java/com/radio/codec2talkie/MainActivity.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/MainActivity.java
@@ -655,9 +655,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 }
