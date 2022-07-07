fdc0ee914e35c1ffe4e0851f6966068c9e0620f9
sh123 <sh123@users.noreply.github.com>
2022-07-07 09:30:45 +0300

    Recorder navigation improvements

--- codec2talkie/src/main/java/com/radio/codec2talkie/recorder/RecorderActivity.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/recorder/RecorderActivity.java
@@ -158,18 +158,23 @@ public class RecorderActivity extends AppCompatActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK  && event.getRepeatCount() == 0) {
-            if (_audioPlayer != null) {
-                _audioPlayer.stopPlayback();
-            }
-            if (!_root.getAbsolutePath().equals(_currentDirectory.getAbsolutePath())) {
-                _currentDirectory = _currentDirectory.getParentFile();
-                if (_currentDirectory != null) {
-                    loadFiles(_currentDirectory);
-                    return true;
-                }
-            }
+            if (loadPreviousDirectory()) return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    private boolean loadPreviousDirectory() {
+        if (_audioPlayer != null) {
+            _audioPlayer.stopPlayback();
+        }
+        if (!_root.getAbsolutePath().equals(_currentDirectory.getAbsolutePath())) {
+            _currentDirectory = _currentDirectory.getParentFile();
+            if (_currentDirectory != null) {
+                loadFiles(_currentDirectory);
+                return true;
+            }
+        }
+        return false;
     }
 
     private void runDeleteFromDirectoryConfirmation(File directory) {
@@ -206,6 +211,7 @@ public class RecorderActivity extends AppCompatActivity {
         int itemId = item.getItemId();
 
         if (itemId == android.R.id.home) {
+            if (loadPreviousDirectory()) return true;
             onBackPressed();
             return true;
         }
