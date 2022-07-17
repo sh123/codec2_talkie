cba37758373344547ecc3d47303bd5efaf88f39e
sh123 <sh123@users.noreply.github.com>
2022-07-17 23:49:58 +0300

    Message view

--- codec2talkie/src/main/java/com/radio/codec2talkie/storage/message/MessageItemActivity.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/storage/message/MessageItemActivity.java
@@ -1,15 +1,39 @@
 package com.radio.codec2talkie.storage.message;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.radio.codec2talkie.R;
+import com.radio.codec2talkie.storage.message.group.MessageGroupViewModel;
 
 public class MessageItemActivity extends AppCompatActivity {
 
-    private MessageItemViewModel _messageItemViewModel;
+    private MessageGroupViewModel _messageGroupViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_view);
+        setTitle(R.string.messages_group_view_title);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int itemId = item.getItemId();
+
+        if (itemId == android.R.id.home) {
+            finish();
+            return true;
+        }
+        else if (itemId == R.id.messages_send) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- codec2talkie/src/main/java/com/radio/codec2talkie/storage/message/group/MessageGroupActivity.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/storage/message/group/MessageGroupActivity.java
@@ -10,16 +10,16 @@ import com.radio.codec2talkie.R;
 
 public class MessageGroupActivity extends AppCompatActivity {
 
-        private MessageGroupViewModel _messageGroupViewModel;
+    private MessageGroupViewModel _messageGroupViewModel;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_message_groups_view);
-            setTitle(R.string.messages_group_view_title);
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_groups_view);
+        setTitle(R.string.messages_group_view_title);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
@@ -28,6 +28,9 @@ public class MessageGroupActivity extends AppCompatActivity {
 
         if (itemId == android.R.id.home) {
             finish();
+            return true;
+        }
+        else if (itemId == R.id.messages_send_to) {
             return true;
         }
         return super.onOptionsItemSelected(item);
