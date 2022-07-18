778fd92655a8c616f469e12b7239a265e03166b6
sh123 <sh123@users.noreply.github.com>
2022-07-18 13:26:38 +0300

    Send to dialog improvements

--- codec2talkie/src/main/java/com/radio/codec2talkie/storage/message/group/MessageGroupDialogSendTo.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/storage/message/group/MessageGroupDialogSendTo.java
@@ -1,16 +1,17 @@
 package com.radio.codec2talkie.storage.message.group;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 import com.radio.codec2talkie.R;
 
-public class MessageGroupDialogSendTo extends Dialog implements View.OnClickListener {
+public class MessageGroupDialogSendTo extends AlertDialog implements View.OnClickListener {
 
     public MessageGroupDialogSendTo(@NonNull Context context) {
         super(context);
@@ -19,11 +20,23 @@ public class MessageGroupDialogSendTo extends Dialog implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Send message to...");
+        setTitle(.getString(R.string.activity_send_message_to_title));
         setContentView(R.layout.activity_send_message_to);
+
+        Button sendButton = findViewById(R.id.send_message_to_btn_ok);
+        assert sendButton != null;
+        sendButton.setOnClickListener(this);
+        Button cancelButton = findViewById(R.id.send_message_to_btn_cancel);
+        assert cancelButton != null;
+        cancelButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.send_message_to_btn_ok) {
+        }  else if (id == R.id.send_message_to_btn_cancel) {
+            dismiss();
+        }
     }
 }
