d7e249f89ade47e3ac457cf07b928e8145a6b078
sh123 <sh123@users.noreply.github.com>
2022-07-20 12:00:15 +0300

    Update repository

--- codec2talkie/src/main/java/com/radio/codec2talkie/storage/log/LogItemDao.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/storage/log/LogItemDao.java
@@ -6,6 +6,8 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
+import com.radio.codec2talkie.storage.message.MessageItem;
+
 import java.util.List;
 
 @Dao
@@ -14,8 +16,17 @@ public interface LogItemDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertLogItem(LogItem logItem);
 
+    @Query("SELECT srcCallsign from LogItem GROUP BY srcCallsign")
+    LiveData<List<String>> getGroups();
+
     @Query("SELECT * FROM LogItem ORDER by timestampEpoch DESC")
     LiveData<List<LogItem>> getAllLogItems();
+
+    @Query("SELECT * FROM LogItem WHERE srcCallsign = :srcCallsign ORDER BY timestampEpoch ASC")
+    LiveData<List<LogItem>> getLogItems(String srcCallsign);
+
+    @Query("DELETE FROM LogItem WHERE srcCallsign = :srcCallsign")
+    void deleteLogItems(String srcCallsign);
 
     @Query("DELETE FROM LogItem")
     void deleteAllLogItems();
--- codec2talkie/src/main/java/com/radio/codec2talkie/storage/log/LogItemRepository.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/storage/log/LogItemRepository.java
@@ -12,24 +12,38 @@ public class LogItemRepository {
 
     private final LogItemDao _logItemDao;
     private final LiveData<List<LogItem>> _logItemLiveData;
+    private LiveData<List<LogItem>> _logItemGroupLiveData;
+    private final LiveData<List<String>> _logItemGroups;
 
     public LogItemRepository(Application application) {
         AppDatabase appDatabase = AppDatabase.getDatabase(application);
         _logItemDao = appDatabase.logItemDao();
         _logItemLiveData = _logItemDao.getAllLogItems();
+        _logItemGroups = _logItemDao.getGroups();
     }
 
-    LiveData<List<LogItem>> getAllLogItems() {
+    public LiveData<List<LogItem>> getAllLogItems() {
         return _logItemLiveData;
     }
 
+    public LiveData<List<String>> getGroups() { return _logItemGroups; }
+
+    public LiveData<List<LogItem>> getLogItems(String groupName) {
+        if (_logItemGroupLiveData == null) {
+            _logItemGroupLiveData = _logItemDao.getLogItems(groupName);
+        }
+        return _logItemGroupLiveData;
+    }
+
     public void insertLogItem(LogItem logItem) {
-        AppDatabase.getDatabaseExecutor().execute(() -> {
-            _logItemDao.insertLogItem(logItem);
-        });
+        AppDatabase.getDatabaseExecutor().execute(() -> _logItemDao.insertLogItem(logItem));
     }
 
     public void deleteAllLogItems() {
         AppDatabase.getDatabaseExecutor().execute(_logItemDao::deleteAllLogItems);
+    }
+
+    public void deleteLogItems(String groupName) {
+        AppDatabase.getDatabaseExecutor().execute(() -> _logItemDao.deleteLogItems(groupName));
     }
 }
