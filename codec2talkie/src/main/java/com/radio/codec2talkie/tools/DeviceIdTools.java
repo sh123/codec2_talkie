0ece911121b0d8acd98321351e35e3eb788ed9f6
sh123 <sh123@users.noreply.github.com>
2025-04-12 22:22:02 +0300

    Mice device ids

--- codec2talkie/src/main/java/com/radio/codec2talkie/tools/DeviceIdTools.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/tools/DeviceIdTools.java
@@ -14,6 +14,7 @@ public class DeviceIdTools {
     private static final String TAG = DeviceIdTools.class.getSimpleName();
     private static final String _deviceIdsAssert = "tocalls.dense.json";
     private static final HashMap<String, String> _deviceIdMap = new HashMap<>();
+    private static final HashMap<String, String> _deviceIdMiceMap = new HashMap<>();
     private static final HashMap<String, String> _deviceIdCache = new HashMap<>();
 
     public static String getDeviceDescription(String deviceId) {
@@ -32,6 +33,10 @@ public class DeviceIdTools {
         return description;
     }
 
+    public static String getMiceDeviceDescription(String miceDeviceId) {
+        return _deviceIdMiceMap.get(miceDeviceId);
+    }
+
     public static void loadDeviceIdMap(Context context) {
         JSONObject jsonObject = loadJSONFromAsset(context);
         if (jsonObject == null) {
@@ -39,22 +44,44 @@ public class DeviceIdTools {
             return;
         }
         try {
+            // load tocalls
             JSONObject tocallsJsonObject = jsonObject.getJSONObject("tocalls");
             Iterator<String> deviceIds = tocallsJsonObject.keys();
             while (deviceIds.hasNext()) {
                 String dbDeviceId = deviceIds.next();
                 JSONObject jsonDeviceEntry = tocallsJsonObject.getJSONObject(dbDeviceId);
-                String model = "Unknown";
-                String vendor = "unknown";
-                if (jsonDeviceEntry.has("model"))
-                    model = jsonDeviceEntry.getString("model");
-                if (jsonDeviceEntry.has("vendor"))
-                    vendor = jsonDeviceEntry.getString("vendor");
+                String description = toDescription(jsonDeviceEntry);
+                if (description == null) continue;
                 String deviceId = dbDeviceId.replaceAll("[?*n]", "");
-                _deviceIdMap.put(deviceId, model + " by " + vendor);
+                _deviceIdMap.put(deviceId, description);
+            }
+            // load mice
+            JSONObject miceJsonObject = jsonObject.getJSONObject("mice");
+            Iterator<String> miceDeviceIds = miceJsonObject.keys();
+            while (miceDeviceIds.hasNext()) {
+                String miceDeviceId = deviceIds.next();
+                JSONObject jsonDeviceEntry = tocallsJsonObject.getJSONObject(miceDeviceId);
+                String description = toDescription(jsonDeviceEntry);
+                if (description == null) continue;
+                _deviceIdMiceMap.put(miceDeviceId, description);
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String toDescription(JSONObject jsonDeviceEntry) {
+        try {
+            String model = "Unknown";
+            String vendor = "unknown";
+            if (jsonDeviceEntry.has("model"))
+                model = jsonDeviceEntry.getString("model");
+            if (jsonDeviceEntry.has("vendor"))
+                vendor = jsonDeviceEntry.getString("vendor");
+            return model + " by " + vendor;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
