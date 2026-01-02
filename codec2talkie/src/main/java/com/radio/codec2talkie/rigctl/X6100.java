0fce46311fc96129a090218fe0029d56314f4105
sh123 <sh123@users.noreply.github.com>
2026-01-02 22:19:43 +0200

    Set Xiegu CIV address to 0xa4

--- codec2talkie/src/main/java/com/radio/codec2talkie/rigctl/X6100.java
+++ codec2talkie/src/main/java/com/radio/codec2talkie/rigctl/X6100.java
@@ -2,5 +2,5 @@ package com.radio.codec2talkie.rigctl;
 
 public class X6100 extends Icom {
     @Override
-    protected byte getCivAddress() { return (byte)0x5E; }
+    protected byte getCivAddress() { return (byte)0xa4; }
 }
